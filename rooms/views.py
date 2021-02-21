bce85f96834f16a7ccca1db84c10038b03e9cc76
DoubleS-Lee <lss8825@gmail.com>
2021-02-21 21:42:21 +0900

    1.4:CBV기반 APIView생성

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,13 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer
 
-# DRF를 사용하기 위해 api_view 데코레이터로 함수를 감싸준다
-# GET, POST, PUT, DELETE 중에 어떤 기능을 사용할지 정해준다
-@api_view(["GET"])
-def list_rooms(request):
-    rooms = Room.objects.all()
-    # queryset 객체인 rooms의 데이터 갯수가 많을 경우 many=True를 붙여줘야 한다
-    serialized_rooms = RoomSerializer(rooms, many=True)
-    # DRF에서 제공하는 response를 return 한다
-    # 이 코드로부터 데이터들을 볼수있는 페이지를 얻게 된다
-    # http://127.0.0.1:8000/api/v1/rooms/list/
-    return Response(data=serialized_rooms.data)
+# APIView를 이용한 Class-Based-View 생성
+# APIView는 DRF에서 제공하는 Apiview중에서 가장 일반적인 형태이다
+# 여기서 한걸음 더 들어가면 ListAPIView, CreateAPIView등을 가지고 있는 Generic View가 있다
+class ListRoomView(APIView):
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
