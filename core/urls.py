0a3af9ccc55ec458944d73a636052de2fa1f5bb6
DoubleS-Lee <lss8825@gmail.com>
2021-02-21 17:41:47 +0900

    1.1:DRF-@api_view,Response

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = "core"
-
-urlpatterns = [
-    path("list", views.list_rooms),
-]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# DRF를 사용하기 위해 api_view 데코레이터로 함수를 감싸준다
+# GET, POST, PUT, DELETE 중에 어떤 기능을 사용할지 정해준다
+@api_view(["GET"])
+def list_rooms(request):
+    # DRF에서 제공하는 response를 return 한다
+    # 이 코드로부터 데이터들을 볼수있는 페이지를 얻게 된다
+    # http://127.0.0.1:8000/api/v1/rooms/list/
+    return Response()
