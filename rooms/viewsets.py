daf46667a0abe3a4fdccd24f69f80bd2b1d2860c
DoubleS-Lee <lss8825@gmail.com>
2021-03-01 12:09:45 +0900

    1.6:viewset 설명 페이지 추가

--- rooms/viewsets.py
+++ rooms/viewsets.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .models import Room
 from .serializers import BigRoomSerializer
 
-
+# https://www.django-rest-framework.org/api-guide/viewsets/#viewset
 # viewsets을 사용하면 get, post, put, delete를 아주 간단하게 구현할 수 있다는 장점이 있음
 # 유저의 종류나 상황에 맞게 get, post, put, delete를 사용해야하는데 이런 조건들을 걸어주기 어려움
 class RoomViewset(viewsets.ModelViewSet):
