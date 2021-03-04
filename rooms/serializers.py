f124a8471d8342ccada31da576250808adcad628
DoubleS-Lee <lss8825@gmail.com>
2021-03-04 20:31:17 +0900

    2.6:UserSerializer 클래스 이름 변경-RelatedUserSerializer

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -2,14 +2,14 @@
 
 from rest_framework import serializers
 from .models import Room
-from users.serializers import UserSerializer
+from users.serializers import RelatedUserSerializer
 
 # 여기서 serializers.Serializer를 쓰면 각각의 데이터에 해당하는 것을 다 일일이 지정해줘야하는데 serializers.ModelSerializer를 쓰면 이게 해결된다
 # serializers.ModelSerializer를 사용한다
 class ReadRoomSerializer(serializers.ModelSerializer):
 
     # room과 foreignkey로 연결되어 있는 user에 있는 데이터들을 불러오려면 이런식으로 users-serializers.py에서 serializer를 생성해주고 이곳으로 불러와서 사용하면 된다
-    user = UserSerializer()
+    user = RelatedUserSerializer()
 
     class Meta:
         model = Room
