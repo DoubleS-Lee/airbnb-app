23ac20c03b0651a335e119b4c25b0935e5cd984c
DoubleS-Lee <lss8825@gmail.com>
2021-03-04 21:12:25 +0900

    2.6:MeView, user_detail 구현(get,put,api_view)

--- users/serializers.py
+++ users/serializers.py
@@ -13,3 +13,18 @@ class RelatedUserSerializer(serializers.ModelSerializer):
             "avatar",
             "superhost",
         )
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            "groups",
+            "user_permissions",
+            "password",
+            "last_login",
+            "is_superuser",
+            "is_staff",
+            "is_active",
+            "date_joined",
+        )
