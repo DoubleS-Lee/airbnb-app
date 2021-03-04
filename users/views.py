9bbb1f7c8d2a704b2eb3167e14895f67eb3ff5b2
DoubleS-Lee <lss8825@gmail.com>
2021-03-04 21:29:41 +0900

    2.7:APIView class에 permission_classes 적용하기

--- users/views.py
+++ users/views.py
@@ -4,12 +4,18 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .models import User
 from .serializers import ReadUserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
+# APIView에 사용하는 장고 authentication, permissions
+# https://www.django-rest-framework.org/api-guide/authentication/
 class MeView(APIView):
+
+    # permission_classes를 사용하면 이 클래스 내부를 실행하기전에 유저가 접근 권한을 가졌는지 자동으로 판별해준다
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        if request.user.is_authenticated:
-            return Response(ReadUserSerializer(request.user).data)
+        return Response(ReadUserSerializer(request.user).data)
 
     def put(self, request):
         pass
