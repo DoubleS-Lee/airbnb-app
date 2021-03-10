3b6410e19fe5cf1b3a850e70e2daca6b411da93f
DoubleS-Lee <lss8825@gmail.com>
2021-03-10 11:50:24 +0900

    2.10:users-serializers.py내 ReadUserSerializer&WriteUserSerializer 합치기(UserSerializer)

--- users/serializers.py
+++ users/serializers.py
@@ -15,28 +15,21 @@ class RelatedUserSerializer(serializers.ModelSerializer):
         )
 
 
-class ReadUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        exclude = (
-            "groups",
-            "user_permissions",
-            "password",
-            "last_login",
-            "is_superuser",
-            "is_staff",
-            "is_active",
-            "date_joined",
-            # user 정보를 보는 곳에서 favs를 불러오면 favs가 1000개 인 경우 곤란한 상황을 맞닥드릴수 있음
-            "favs",
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost",
         )
 
+        read_only_fields = ("id", "superhost", "avatar")
 
-class WriteUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("username", "first_name", "last_name", "email")
-
-    def validate_first_name(self, value):
+        def validate_first_name(self, value):
         # print(value)
         return value.upper()
--- users/views.py
+++ users/views.py
@@ -3,9 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import User
-from .serializers import ReadUserSerializer
 from rest_framework.permissions import IsAuthenticated
-from .serializers import WriteUserSerializer
+from .serializers import UserSerializer
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
 
@@ -18,10 +17,10 @@ class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response(ReadUserSerializer(request.user).data)
+        return Response(UserSerializer(request.user).data)
 
     def put(self, request):
-        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
         # print(serializer.is_valid())
         if serializer.is_valid():
             serializer.save()
@@ -34,7 +33,7 @@ class MeView(APIView):
 def user_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
-        return Response(ReadUserSerializer(user).data)
+        return Response(UserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
